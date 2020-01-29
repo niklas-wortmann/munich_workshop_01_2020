e3b6f810dad26d52d5aa5d7b073f58ae9f2550f4
Jan-Niklas Wortmann <6104311+JWO719@users.noreply.github.com>
2020-01-29 11:23:37 +0100

    excercise 02 takeuntil approach

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { BooksService, Book } from "../books.service";
-import { Observable } from "rxjs";
+import { Observable, Subscription, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
   selector: "app-books-list",
   templateUrl: "./books-list.component.html",
   styleUrls: ["./books-list.component.scss"]
 })
-export class BooksListComponent implements OnInit {
-  books$: Observable<Book[]>;
-
+export class BooksListComponent implements OnInit, OnDestroy {
+  books: Book[];
+  sub: Subscription;
+  destroy$ = new Subject<void>();
   constructor(private bookService: BooksService) {}
 
   ngOnInit() {
-    this.books$ = this.bookService.fetchData();
+    this.sub = this.bookService
+      .fetchData()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(data => (this.books = data));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
   }
 }
