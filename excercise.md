be37067cfcc932d4bab81684f04c0d1458add194
Jan-Niklas Wortmann <6104311+JWO719@users.noreply.github.com>
2020-01-29 10:44:03 +0100

    solution excercise 1

---
+++ excercise.md
@@ -0,0 +1,20 @@
+# 29.01.2020 Munich
+
+## Excercise 01
+
+0. Bookmonkey API starten
+
+```sh
+bookmonkey-api
+```
+
+1. Neue Angular Anwednung bootstrappen
+
+```sh
+ng new ...
+```
+
+2. Ein Angular Service implementieren, dass die Booksmonkey API anspricht und in einem BehaviorSubject speichert.
+
+HINT:
+GET http://localhost:4730/books
---
+++ src/app/books.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+
+interface Book {
+  title: string;
+}
+
+@Injectable({
+  providedIn: "root"
+})
+export class BooksService {
+  private store = new BehaviorSubject<Book[]>(null);
+
+  get books$(): Observable<Book[]> {
+    return this.store.asObservable();
+  }
+
+  constructor(private httpClient: HttpClient) {}
+
+  fetchData(): Observable<Book[]> {
+    return this.httpClient.get<Book[]>("http://localhost:4730/books").pipe(
+      tap(data => {
+        this.store.next(data);
+      })
+    );
+  }
+}
